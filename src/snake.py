a12169b9a9ab9b224de0da6e739298297dbc0ef8
katacora <124920949+katacora@users.noreply.github.com>
2023-10-25 17:15:20 +0200

    Update snake.py
    gameover ventana

--- src/snake.py
+++ src/snake.py
@@ -2,102 +2,118 @@ import sys
 import random
 import pygame
 
-# Initialize Pygame
+# Inicializar Pygame
 pygame.init()
 
-# Set up the game window
-WINDOW_WIDTH = 800
-WINDOW_HEIGHT = 600
-window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+# Configurar la ventana del juego
+ANCHO_VENTANA = 800
+ALTO_VENTANA = 600
+ventana = pygame.display.set_mode((ANCHO_VENTANA, ALTO_VENTANA))
 pygame.display.set_caption("Snake")
 
-# Set up the game clock
-clock = pygame.time.Clock()
+# Configurar el reloj del juego
+reloj = pygame.time.Clock()
 
-# Set up the snake
-SNAKE_SIZE = 20
-snake = [pygame.Rect(WINDOW_WIDTH // 2, WINDOW_HEIGHT // 2, SNAKE_SIZE, SNAKE_SIZE)]
-snake_direction = "right"
+# Configurar la serpiente
+TAMANO_SERPIENTE = 20
+serpiente = [pygame.Rect(ANCHO_VENTANA // 2, ALTO_VENTANA // 2, TAMANO_SERPIENTE, TAMANO_SERPIENTE)]
+direccion_serpiente = "derecha"
 
-# Set up the food
-FOOD_SIZE = 20
-food = pygame.Rect(random.randint(0, WINDOW_WIDTH - FOOD_SIZE), random.randint(0, WINDOW_HEIGHT - FOOD_SIZE), FOOD_SIZE, FOOD_SIZE)
+# Configurar la comida
+TAMANO_COMIDA = 20
+comida = pygame.Rect(random.randint(0, ANCHO_VENTANA - TAMANO_COMIDA), random.randint(0, ALTO_VENTANA - TAMANO_COMIDA), TAMANO_COMIDA, TAMANO_COMIDA)
 
-# Set up the score
-score = 0
-font = pygame.font.SysFont(None, 30)
+# Inicializar el estado del juego
+game_over = False
 
+# Función para mostrar un mensaje de "Game Over"
+def mostrar_game_over():
+    fuente = pygame.font.Font(None, 36)
+    texto_game_over = fuente.render("Game Over - Presiona R para reiniciar", True, (255, 255, 255))
+    rectangulo_game_over = texto_game_over.get_rect(center=(ANCHO_VENTANA // 2, ALTO_VENTANA // 2))
+    ventana.blit(texto_game_over, rectangulo_game_over)
+    pygame.display.flip()
 
-# Game loop
-while True:
-    # Handle events
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+# Bucle del juego
+while not game_over:
+    for evento in pygame.event.get():
+        if evento.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
 
-    # Move the snake
-    if snake_direction == "right":
-        new_head = pygame.Rect(snake[-1].x + SNAKE_SIZE, snake[-1].y, SNAKE_SIZE, SNAKE_SIZE)
-    elif snake_direction == "left":
-        new_head = pygame.Rect(snake[-1].x - SNAKE_SIZE, snake[-1].y, SNAKE_SIZE, SNAKE_SIZE)
-    elif snake_direction == "up":
-        new_head = pygame.Rect(snake[-1].x, snake[-1].y - SNAKE_SIZE, SNAKE_SIZE, SNAKE_SIZE)
-    elif snake_direction == "down":
-        new_head = pygame.Rect(snake[-1].x, snake[-1].y + SNAKE_SIZE, SNAKE_SIZE, SNAKE_SIZE)
+    if not game_over:
+        # Manejar la entrada del jugador
+        teclas = pygame.key.get_pressed()
+        if teclas[pygame.K_RIGHT] and direccion_serpiente != "izquierda":
+            direccion_serpiente = "derecha"
+        elif teclas[pygame.K_LEFT] and direccion_serpiente != "derecha":
+            direccion_serpiente = "izquierda"
+        elif teclas[pygame.K_UP] and direccion_serpiente != "abajo":
+            direccion_serpiente = "arriba"
+        elif teclas[pygame.K_DOWN] and direccion_serpiente != "arriba":
+            direccion_serpiente = "abajo"
 
-    # Check if the snake hit the wall
-    if new_head.left < 0:
-        new_head.left = WINDOW_WIDTH - SNAKE_SIZE
-    elif new_head.right > WINDOW_WIDTH:
-        new_head.left = 0
-    elif new_head.top < 0:
-        new_head.top = WINDOW_HEIGHT - SNAKE_SIZE
-    elif new_head.bottom > WINDOW_HEIGHT:
-        new_head.top = 0
+        # Mover la serpiente
+        if direccion_serpiente == "derecha":
+            nueva_cabeza = pygame.Rect(serpiente[-1].x + TAMANO_SERPIENTE, serpiente[-1].y, TAMANO_SERPIENTE, TAMANO_SERPIENTE)
+        elif direccion_serpiente == "izquierda":
+            nueva_cabeza = pygame.Rect(serpiente[-1].x - TAMANO_SERPIENTE, serpiente[-1].y, TAMANO_SERPIENTE, TAMANO_SERPIENTE)
+        elif direccion_serpiente == "arriba":
+            nueva_cabeza = pygame.Rect(serpiente[-1].x, serpiente[-1].y - TAMANO_SERPIENTE, TAMANO_SERPIENTE, TAMANO_SERPIENTE)
+        elif direccion_serpiente == "abajo":
+            nueva_cabeza = pygame.Rect(serpiente[-1].x, serpiente[-1].y + TAMANO_SERPIENTE, TAMANO_SERPIENTE, TAMANO_SERPIENTE)
 
+        # Comprobar colisiones con la pared
+        if (
+            nueva_cabeza.left < 0
+            or nueva_cabeza.right > ANCHO_VENTANA
+            or nueva_cabeza.top < 0
+            or nueva_cabeza.bottom > ALTO_VENTANA
+        ):
+            game_over = True
 
-    # Check if the snake hit itself
-    if new_head in snake:
-        pygame.quit()
-        sys.exit()
+        # Comprobar colisiones con la serpiente
+        if nueva_cabeza in serpiente:
+            game_over = True
 
-    # Check if the snake ate the food
-    if new_head.colliderect(food):
-        food.x = random.randint(0, WINDOW_WIDTH - FOOD_SIZE)
-        food.y = random.randint(0, WINDOW_HEIGHT - FOOD_SIZE)
-        score += 1
-    else:
-        snake.pop(0)
+        # Comprobar si la serpiente ha comido la comida
+        if nueva_cabeza.colliderect(comida):
+            comida.x = random.randint(0, ANCHO_VENTANA - TAMANO_COMIDA)
+            comida.y = random.randint(0, ALTO_VENTANA - TAMANO_COMIDA)
+        else:
+            serpiente.pop(0)
 
-    # Add the new head to the snake
-    snake.append(new_head)
+        # Agregar la nueva cabeza a la serpiente
+        serpiente.append(nueva_cabeza)
 
-    # Handle input
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_RIGHT] and snake_direction != "left":
-        snake_direction = "right"
-    elif keys[pygame.K_LEFT] and snake_direction != "right":
-        snake_direction = "left"
-    elif keys[pygame.K_UP] and snake_direction != "down":
-        snake_direction = "up"
-    elif keys[pygame.K_DOWN] and snake_direction != "up":
-        snake_direction = "down"
+        # Borrar la pantalla
+        ventana.fill((0, 0, 0))
 
-    # Clear the screen
-    window.fill((0, 0, 0))
+        # Dibujar la serpiente y la comida
+        for segmento in serpiente:
+            pygame.draw.rect(ventana, (255, 255, 255), segmento)
+        pygame.draw.rect(ventana, (255, 0, 0), comida)
 
-    # Draw the snake and food
-    for segment in snake:
-        pygame.draw.rect(window, (255, 255, 255), segment)
-    pygame.draw.rect(window, (255, 0, 0), food)
+        # Actualizar la pantalla
+        pygame.display.flip()
 
-        # Draw the score
-    score_text = font.render("Score: " + str(score), True, (255, 255, 255))
-    window.blit(score_text, (10, 10))
+        # Limitar la velocidad de fotogramas
+        reloj.tick(10)
 
-    # Update the display
-    pygame.display.flip()
+    # Mostrar "Game Over" si el juego ha terminado
+    while game_over:
+        for evento in pygame.event.get():
+            if evento.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if evento.type == pygame.KEYDOWN:
+                if evento.key == pygame.K_r:
+                    # Reiniciar el juego
+                    serpiente = [pygame.Rect(ANCHO_VENTANA // 2, ALTO_VENTANA // 2, TAMANO_SERPIENTE, TAMANO_SERPIENTE)]
+                    direccion_serpiente = "derecha"
+                    comida.x = random.randint(0, ANCHO_VENTANA - TAMANO_COMIDA)
+                    comida.y = random.randint(0, ALTO_VENTANA - TAMANO_COMIDA)
+                    game_over = False
 
-    # Limit the frame rate
-    clock.tick(20)
+        mostrar_game_over()
+
